4bdb476d062c176ab3f7e6065d6ede47907b2fa9
xx <b@c.net>
2022-04-24 23:28:23 +0800

    dynamic

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "FileLogging",
+            type: .dynamic,
             targets: ["FileLogging"]),
     ],
     dependencies: [
